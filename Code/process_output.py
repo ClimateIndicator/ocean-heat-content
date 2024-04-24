8eccc7aed5c885868a0e675d880ce8c89f140568
Chris Smith <chrisroadmap@gmail.com>
2024-04-24 10:12:48 +0100

    Add Matt's 2023 update code

--- Code/process_output.py
+++ Code/process_output.py
@@ -3,8 +3,8 @@ import pickle
 import numpy as np
 import pandas as pd
 
-with open('../Output/IPCC_AR6_update_energy_timeseries_1971to2022_2023-04-17.pickle', 'rb') as filename:
+with open('../Output/IGCC_AR6_update_energy_timeseries_1971to2023_2024-04-13.pickle', 'rb') as filename:
     data = pickle.load(filename)
 
 df = pd.DataFrame(data)
-df.to_csv('../Output/IPCC_AR6_update_energy_timeseries_1971to2022_2023-04-17.csv', index=False)
+df.to_csv('../Output/IGCC_AR6_update_energy_timeseries_1971to2023_2024-04-13.csv', index=False)
